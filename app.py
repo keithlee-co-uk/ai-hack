157a3847d1ebc227011254284b128047e6b36218
Keith Lee <code@keithlee.co.uk>
2024-12-12 23:17:19 +0000

    Added the code

--- app.py
+++ app.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
-import settings
 
-key = settings.key
+from openai import OpenAI
+import os
+# from flask import Flask, render_template, request, flash
+# from flask import redirect, url_for, Request
+# import pandas as pd
+import time
+
+import settings
+logger = settings.logger
+
+# app = Flask(__name__)
+# app.secret_key = 'session_secret_key'
+
+client = OpenAI(
+    api_key = settings.key
+
+)
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+        )
+
+    return response.choices[0].message["content"]
+
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+
+    prompt = "What do you know about me?"
+
+    answer = get_completion(prompt)
+    logger.info(answer)
+    print(answer)
--- settings.py
+++ settings.py
@@ -1,1 +1,16 @@
-key = <replace with api key>
+from logging import basicConfig, DEBUG, INFO, getLogger
+
+debug = True
+
+level = INFO
+if debug:
+    level = DEBUG
+
+logging_file = "logs/ai-hack.log"
+basicConfig(filename=logging_file,
+            format='%(asctime)s (%(levelname)s) %(message)s',
+            level=DEBUG,
+            datefmt='%d.%m.%Y %H:%M:%S')
+logger = getLogger(__name__)
+
+key = "<replace with api key>"
